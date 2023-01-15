7c47d8852ad5613b200fa3273d8ef77f9dd48b0f
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-01-15 20:29:16 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import './css/styles.css';
+import './fetchCountries';
+import debounce from 'lodash.debounce';
+import fetchCountries from './fetchCountries';
 
+const inputHandle = document.querySelector('#search-box');
+const countryListRef = document.querySelector('.country-list');
+const countryInfoRef = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
+
+inputHandle.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+
+function onInputSearch(event) {
+  event.preventDefault();
+  const input = inputHandle.value.trim();
+  if (input === '') {
+    return;
+  } else {
+    fetchCountries(input).then(result =>
+      result.map(country => createCountriesMarkup(country))
+    );
+  }
+}
+
+function createCountriesMarkup(country) {
+  const markup = `<div class="country-info">
+      <div>
+        <svg>
+          <use href="${country.flags.svg}"></use>
+         </svg>
+         <p>${country.name.official}</p>
+      </div>
+      <p>Capital: ${country.capital}</p>
+      <p>Population: ${country.population}</p>
+      <p>Languages: ${country.languages}</p>
+    </div>`;
+  countryListRef.insertAdjacentHTML('beforeend', markup);
+  console.log(country.languages);
+}
