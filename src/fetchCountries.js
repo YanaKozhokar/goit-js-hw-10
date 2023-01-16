e52e22e305766d3403417de1f853222d2d2a6268
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-01-16 18:43:41 +0200

    updated

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,3 @@
-import Notiflix from 'notiflix';
 export default function fetchCountries(name) {
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
